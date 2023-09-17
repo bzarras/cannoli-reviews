0e7c7a71cd6f4a54e9bc03050beef27059adc1a6
Ben Zarras <benzarras@gmail.com>
2023-09-17 16:07:41 -0400

    better badge display

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 import datetime
-from pydantic import BaseModel, field_serializer
+from pydantic import BaseModel, computed_field, field_serializer
 
 
 class Review(BaseModel):
@@ -13,6 +13,20 @@ class Review(BaseModel):
     rating: float
     img_url: str
     tags: list[str] = []
+
+    @computed_field
+    @property
+    def badge_color(self) -> str:
+        if self.rating >= 9.0:
+            return "#3DA13D"
+        elif self.rating >= 8.0:
+            return "#78CC78"
+        elif self.rating >= 7.0:
+            return "#FFE536"
+        elif self.rating >= 6.0:
+            return "#FFF29C"
+        else:
+            return "#FF5368"
 
     @field_serializer('date')
     def serialize_date(self, date: datetime.date, _info):
